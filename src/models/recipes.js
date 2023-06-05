61e802688573440a11119e24230bfad8a902c273
DynnoOttu <dynnoottu968@gmail.com>
2023-06-06 00:57:27 +0800

    photo users

--- src/models/recipes.js
+++ src/models/recipes.js
@@ -11,7 +11,7 @@ const insertData = (data) => {
 const getData = (data) => {
   const { searchBy, search, sortBy, sort } = data
   return Pool.query(
-    `SELECT recipes.id, recipes.title,recipes.ingredients,recipes.created_at as posttime, category.name as category, recipes.photo, users.fullname as creator, users.email FROM recipes JOIN category ON recipes.category_id=category.id JOIN users ON recipes.users_id=users.id WHERE recipes.${searchBy} ILIKE '%${search}%' AND recipes.deleted_at IS NULL ORDER BY recipes.${sortBy} ${sort} LIMIT 10`
+    `SELECT recipes.id, recipes.title,recipes.ingredients,recipes.created_at as posttime, category.name as category, recipes.photo, users.fullname as creator, users.photo as photo_users, users.email FROM recipes JOIN category ON recipes.category_id=category.id JOIN users ON recipes.users_id=users.id WHERE recipes.${searchBy} ILIKE '%${search}%' AND recipes.deleted_at IS NULL ORDER BY recipes.${sortBy} ${sort} LIMIT 10`
   )
 }
 
